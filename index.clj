baf422e1098eb17caef76ef176a8c3e0c1716fcb
Kunwei Zhang <kunweiz92@gmail.com>
2023-12-05 02:34:59 +0800

    hack

--- index.clj
+++ index.clj
@@ -3,6 +3,7 @@
   (:require common)
   (:require [nextjournal.clerk :as clerk]))
 
+^{:nextjournal.clerk/no-cache true}
 (clerk/html
  (into [:div.md:grid.md:gap-8.md:grid-cols-1.pb-8]
        (map (fn [path] [:a {:href (clerk/doc-url path)} path])
