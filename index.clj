f776c2f6b7072f503b81240fa9eeaa60c24109c0
Kunwei Zhang <kunweiz92@gmail.com>
2023-12-05 12:43:03 +0800

    hack

--- index.clj
+++ index.clj
@@ -2,11 +2,12 @@
 (ns index
   (:require [common]
             [nextjournal.clerk :as clerk]))
-{:nextjournal.clerk/visibility {:code :hide}}
+{::clerk/visibility {:code :hide}}
 (clerk/html [:style (slurp "common.css")])
 
 (clerk/md "## 张昆玮的小站\n\n# USACO 解题练习\n\n")
 
+^{::clerk/no-cache true}
 (clerk/html
  (list [:h2 "铜组"]
        (into [:ul.m-0]
